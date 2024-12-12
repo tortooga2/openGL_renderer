16a27f2ccb552cf7ec5c6196ccd4b1770a419e38
Chase Nagle <50192480+tortooga2@users.noreply.github.com>
2024-12-12 12:35:22 -0800

    Delete build directory
    deleted build directory

--- build/macos-app/CMakeFiles/Nurture.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Nurture.
-# This may be replaced when dependencies are built.
--- build/macos-app/CMakeFiles/always_copy_files.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for always_copy_files.
